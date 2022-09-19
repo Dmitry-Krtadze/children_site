be9c2b02c7834b40bf227899b274ec887ccae1fa
Dmitry-Krtadze <73993967+Dmitry-Krtadze@users.noreply.github.com>
2022-09-19 22:31:35 +0300

    First commit

---
+++ school site/js/main.js
@@ -0,0 +1,49 @@
+//Pup up
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+});
+
+
+
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1) {
+		$("header").addClass("sticky");
+	} else {
+		$("header").removeClass("sticky");
+	}
+});
+
+
+
+
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
